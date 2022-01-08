5407129029bb77698f240b2ac5c8853c59683862
@AmalAbdullaev <AAbdullaev@alfabank.ru>
2022-01-09 01:56:05 +0300

    feat: 🎸 сделал мобильную версию хедера

--- src/packages/modules/Header/Header.tsx
+++ src/packages/modules/Header/Header.tsx
@@ -15,65 +15,65 @@ const Header: VFC = () => {
 
   return (
     <header className={styles.header}>
-      <input id="burger" type="checkbox" className={styles.header_hamburger_input} />
-      <label className={styles.header_hamburger} htmlFor="burger">
-        <div className={clsx(styles.top,styles.item)}></div>
-        <div className={clsx(styles.meat,styles.item)}></div>
-        <div className={clsx(styles.bottom,styles.item)}></div>
-      </label>
-      <a href="#" className={styles.logo}>
-        { logo }
-      </a>
-      <a href="#" className={styles.mobileLogo}>
-        { mobileLogo }
-      </a>
-      <div className={styles.header_menu}>
-        <Link href="#" passHref >
-          <a
-            onMouseMove={() => cursorChangeHandler('hovered')}
-            onMouseLeave={() => cursorChangeHandler('')}
-            className={styles.header_menu_item}>
-            {home}
-            Home
-          </a>
-        </Link>
-        <Link href="#" passHref >
-          <a
-            onMouseMove={() => cursorChangeHandler('hovered')}
-            onMouseLeave={() => cursorChangeHandler('')}
-            className={styles.header_menu_item}>
-            co-creative ethos
-          </a>
-        </Link>
-        <Link href="#" passHref>
-          <a
-            onMouseMove={() => cursorChangeHandler('hovered')}
-            onMouseLeave={() => cursorChangeHandler('')}
-            className={styles.header_menu_item}>
-              team
-          </a>
-        </Link>
-        <Link href="#" passHref>
-          <a
-            onMouseMove={() => cursorChangeHandler('hovered')}
-            onMouseLeave={() => cursorChangeHandler('')}
-            className={styles.header_menu_item}>
-            roadmap
-          </a>
-        </Link>
-        <Link href="#" passHref>
-          <a
-            onMouseMove={() => cursorChangeHandler('hovered')}
-            onMouseLeave={() => cursorChangeHandler('')}
-            className={styles.header_menu_item}>
-            f.a.q
-          </a>
-        </Link>
-      </div>
-      <div className={styles.header_aside}>
-        <a href="#" className={styles.header_aside_item}>{twitter}</a>
-        <a href="#" className={styles.header_aside_item}>{facebook}</a>
-      </div>
+        <input id="burger" type="checkbox" className={styles.header_hamburger_input} />
+        <label className={styles.header_hamburger} htmlFor="burger">
+          <div className={clsx(styles.top,styles.item)}></div>
+          <div className={clsx(styles.meat,styles.item)}></div>
+          <div className={clsx(styles.bottom,styles.item)}></div>
+        </label>
+        <a href="#" className={styles.logo}>
+          { logo }
+        </a>
+        <a href="#" className={styles.mobileLogo}>
+          { mobileLogo }
+        </a>
+        <div className={styles.header_menu}>
+          <Link href="#" passHref >
+            <a
+              onMouseMove={() => cursorChangeHandler('hovered')}
+              onMouseLeave={() => cursorChangeHandler('')}
+              className={styles.header_menu_item}>
+              {home}
+              Home
+            </a>
+          </Link>
+          <Link href="#" passHref >
+            <a
+              onMouseMove={() => cursorChangeHandler('hovered')}
+              onMouseLeave={() => cursorChangeHandler('')}
+              className={styles.header_menu_item}>
+              co-creative ethos
+            </a>
+          </Link>
+          <Link href="#" passHref>
+            <a
+              onMouseMove={() => cursorChangeHandler('hovered')}
+              onMouseLeave={() => cursorChangeHandler('')}
+              className={styles.header_menu_item}>
+                team
+            </a>
+          </Link>
+          <Link href="#" passHref>
+            <a
+              onMouseMove={() => cursorChangeHandler('hovered')}
+              onMouseLeave={() => cursorChangeHandler('')}
+              className={styles.header_menu_item}>
+              roadmap
+            </a>
+          </Link>
+          <Link href="#" passHref>
+            <a
+              onMouseMove={() => cursorChangeHandler('hovered')}
+              onMouseLeave={() => cursorChangeHandler('')}
+              className={styles.header_menu_item}>
+              f.a.q
+            </a>
+          </Link>
+        </div>
+        <div className={styles.header_aside}>
+          <a href="#" className={styles.header_aside_item}>{twitter}</a>
+          <a href="#" className={styles.header_aside_item}>{facebook}</a>
+        </div>
     </header>
   )
 }
