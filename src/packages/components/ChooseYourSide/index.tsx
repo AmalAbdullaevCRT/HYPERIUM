0d51d8bbecac1c09528d7bd07a46b6638cc039b7
@AmalAbdullaev <AAbdullaev@alfabank.ru>
2022-01-12 20:45:39 +0300

    feat: 🎸 исправил shoose you side

--- src/packages/components/ChooseYourSide/index.tsx
+++ src/packages/components/ChooseYourSide/index.tsx
@@ -1,54 +1,40 @@
-import { useLayoutEffect, useState, VFC } from 'react'
-import { useMediaQuery } from 'react-responsive'
+import { useEffect, useState, VFC } from 'react'
 import { Element,scroller } from 'react-scroll'
 
 import ChooseYourSideItem from './ChooseYourSideItem'
 import styles from './styles.module.scss'
 
 const ChooseYourSide: VFC = () => {
-  const [historyStyles, setHistoryStyles] = useState({
-    maxHeight: 420
-  })
-
-  const handleMediaQueryChange = (isMobile) => {
-    setHistoryStyles({
-      maxHeight: isMobile? 320 : 420
-    })
+  const [opened, setOpened] = useState([])
+  const chooseYourSideHandler = () => {
+    const newState = [...opened]
+    newState.push(true)
+    setOpened(newState)
   }
 
-  const isMobile = useMediaQuery({ maxWidth: 767 }, undefined,  handleMediaQueryChange)
-
-  const chooseYourSideHandler = (idx: number) => {
-    setHistoryStyles({
-      maxHeight: historyStyles.maxHeight + (isMobile? 440 : 520)
-    })
-    scroller.scrollTo(`choose-your-side-${idx}`, {
+  useEffect(() => {
+    scroller.scrollTo(`choose-your-side-${opened.length - 1}`, {
       duration: 1000,
-      delay: 100,
+      delay: 0,
       smooth: 'easeInOutQuart'
     })
-  }
+  }, [opened])
 
-  useLayoutEffect(() => {
-    setHistoryStyles({
-      maxHeight: isMobile? 320 : 420
-    })
-  }, [isMobile])
 
   return (
     <div className={styles.main}>
       <div className={styles.main_title}>
         CHOOSE YOUR SIDE
       </div>
-      <div className={styles.main_history} style={
-        { maxHeight : `${historyStyles.maxHeight}px` }
-      }>
-        <Element onClick={() => chooseYourSideHandler(1)}>
+      <div className={styles.main_history}>
+        <Element onClick={() => chooseYourSideHandler()} >
           <ChooseYourSideItem
             leftImgURL="/static/images/fortnite_item1.png" 
             rightImgURL="/static/images/fortnite_item1.png" />
         </Element>
-        <Element onClick={() => chooseYourSideHandler(2)} name='choose-your-side-1'>
+        {opened[0] && <Element onClick={() => chooseYourSideHandler()}  
+          name='choose-your-side-0' 
+          className={opened[0] ? styles.inAnimation : styles.outAnimation}>
           <ChooseYourSideItem
             title='History'
             description='Divine Anarchy is the first attempt at an in game 
@@ -57,9 +43,11 @@ const ChooseYourSide: VFC = () => {
             number={1}
             leftImgURL="/static/images/fortnite_item1.png" 
             rightImgURL="/static/images/fortnite_item1.png" />
-        </Element>
-        <Element onClick={() => chooseYourSideHandler(3)} name='choose-your-side-2'>
-          <ChooseYourSideItem
+        </Element>}
+        {opened[1] && <Element onClick={() => chooseYourSideHandler()} 
+          name='choose-your-side-1' 
+          className={opened[1] ? styles.inAnimation : styles.outAnimation}>
+        <ChooseYourSideItem
             title='History'
             description='Divine Anarchy is the first attempt at an in game 
               governance NFT that will act as an experimental 
@@ -67,8 +55,10 @@ const ChooseYourSide: VFC = () => {
             number={2}
             leftImgURL="/static/images/fortnite_item1.png" 
             rightImgURL="/static/images/fortnite_item1.png" />
-        </Element>
-        <Element  name='choose-your-side-3'>
+        </Element>}
+        {opened[2] && <Element  
+        name='choose-your-side-2'
+        className={opened[2] ? styles.inAnimation : styles.outAnimation}>
           <ChooseYourSideItem
             title='History'
             description='Divine Anarchy is the first attempt at an in game 
@@ -77,7 +67,7 @@ const ChooseYourSide: VFC = () => {
             number={3}
             leftImgURL="/static/images/fortnite_item1.png" 
             rightImgURL="/static/images/fortnite_item1.png" />
-        </Element>
+        </Element>}
       </div>
     </div>
   )
